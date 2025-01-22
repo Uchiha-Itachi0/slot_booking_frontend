f3d99db32943b3b726d14b6e0332f3aabd29571b
Anubhav Shukla <anubhav008shukla@gmail.com>
2025-01-22 11:22:18 +0530

    Initialize the project and install the basic packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#804dee",
+        secondary: "#4b42a7",
+        tertiary: "#9b4dee",
+        quaternary: "#4d9aee",
+        five: "#cbb8f5",
+
+        ctnPrimaryLight: "#2e384d",
+        ctnSecondaryLight: "#4b42a7",
+
+        // bgPrimaryDark: "#181826",
+        // bgSecondaryDark: "#212134",
+        ctnPrimaryDark: "#e5e6e9",
+        ctnSecondaryDark: "#7e8c9f"
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35"
       },
     },
   },
